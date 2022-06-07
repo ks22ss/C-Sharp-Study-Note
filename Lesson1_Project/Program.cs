b7b6b0161f371476a6297db1a7c04c06acb74ee1
kingson299 <kingson22.work@gmail.com>
2022-06-07 22:44:45 +0800

    update

---
+++ Lesson1_Project/Program.cs
@@ -0,0 +1,30 @@
+﻿// We are building a calculator using C# console
+
+System.Console.WriteLine("Input the 1st number:");
+string? a = System.Console.ReadLine();
+System.Console.WriteLine("Input the 2nd number:");
+string? b = System.Console.ReadLine();
+
+
+System.Console.WriteLine($"The answer is {a+b}");
+
+
+//Explicit Type Conversion Integer only
+System.Console.WriteLine("Input the 1st number(integer):");
+int num1 = System.Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine("Input the 2nd number(integer):");
+int num2 = System.Convert.ToInt32(System.Console.ReadLine());
+
+
+System.Console.WriteLine($"The answer is {num1 + num2}");
+
+
+//Explicit Type Conversion Integer and Double
+System.Console.WriteLine("Input the 1st number(integer):");
+double num3 = System.Convert.ToDouble(System.Console.ReadLine());
+System.Console.WriteLine("Input the 2nd number(integer):");
+double num4 = System.Convert.ToDouble(System.Console.ReadLine());
+
+
+System.Console.WriteLine($"The answer is {num3 + num4}");
+
